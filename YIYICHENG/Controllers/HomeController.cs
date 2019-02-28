287b2efcff0d0ef854e6e7f97478fa75604a6317
bai <123@qq.com>
2019-02-28 22:15:39 +0800

    提交商城项目结构

---
+++ YIYICHENG/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YIYICHENG.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //登入页面
+        public ActionResult Login()
+        {
+            return View();
+        }
+        //注册页面
+        public ActionResult Regster()
+        {
+            return View();
+        }
+
+        //产品列表
+        public ActionResult Product()
+        {
+            return View();
+        }
+
+        //购物车
+        public ActionResult ShoppingCat()
+        {
+            return View();
+        }
+    }
+}
